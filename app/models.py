147c45c694bd0e40fcf00f74c9aca6451aab6c57
vishalsookram44 <vishalsookram44@gmail.com>
2026-04-23 00:42:28 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,3 +9,14 @@ class User(SQLModel, table=True):
     username:str = Field(index=True, unique=True)
     email:str = Field(index=True, unique=True)
     password:str
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password = password_hash.hash(password)
+
+    def __str__(self) -> str:
+        return f"(User id {self.id}, username={self.username}, email={self.email})"
